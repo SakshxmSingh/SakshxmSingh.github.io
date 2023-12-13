836cab7dc1382fe17ad854898e8b8bb885514e6c
Saksham Singh <saksham5904@gmail.com>
2023-12-13 13:33:39 +0530

    header

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    fs.readFile('index.html', (err, data) => {
+        if (err) {
+            res.writeHead(404);
+            res.end(JSON.stringify(err));
+            return;
+        }
+        res.writeHead(200);
+        res.end(data);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://localhost:3000/');
+});
